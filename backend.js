0a24d181e0b79a7e16581d92579bdba2fb296fdb
Boris-Andrei Pavel <borisandreipavel@gmail.com>
2025-10-16 19:49:16 +0300

    added cors

--- backend.js
+++ backend.js
@@ -1,7 +1,10 @@
 import express from "express";
 import fetch from "node-fetch";
+import cors from "cors";
+
 
 const app = express();
+app.use(cors());
 const PORT = 3000;
 
 // replace with your credentials (keep them secret here)
